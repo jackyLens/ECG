8e0b6ddb0870640cada3c336bcb0b5115bc40820
LawlietTsh <lawliet4062@gmail.com>
2017-03-31 19:39:31 +0800

    Delete

--- MIT_BIH_loaddata.m
+++
@@ -1,8 +0,0 @@
-function [train_dataset, val_dataset, test_dataset, num_train, num_val, num_test] = MIT_BIH_loaddata()
-
-load MIT_BIH_data.mat
-
-train_dataset = MIT_BIH_data_train;
-val_dataset = MIT_BIH_data_val;
-test_dataset = MIT_BIH_data_test;
-
--- my_resample.m
+++
@@ -1,7 +0,0 @@
-function out = my_resample(in)
-p = 540 * 1.1;
-[~,q] = size(in);
-out = resample(in,p,q);
-out = out(1:540);
-end
-
